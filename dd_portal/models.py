5a79154f5e9d2c4ab8ef029c3dbb7c30c6dd3f9c
Emmanuel <info@regularoddity.com>
2013-10-24 00:48:03 -0700

    Added relations between models.

--- dd_portal/models.py
+++ dd_portal/models.py
@@ -4,34 +4,50 @@ from django.contrib.auth.models import User
 class Student(models.Model):
     '''
     A student profile has:
+    * a link to a Django user
     * a single parent accound linked to it
     * any number of classes
     * a school grade (for an enum)
+    TODO: make sure that a student can never be both actively in a course and
+          dropped from it.
     '''
-    user = models.OneToOneField(User)
-    # parent
-    # classes
-    # grade
+    user            = models.OneToOneField(User)
+    parent          = models.ForeignKey(Parent)
+    active_courses  = models.ManyToManyField(Course, related_name='active_enrolment')
+    dropped_courses = models.ManyToManyField(Course, related_name='dropped_enrolment')
+    grade           = models.ForeignKey(Grade)
+
 
 class Parent(models.Model):
     '''
     A parent profile has:
-    * any number of student accounts linked to it.
+    * a link to a Django user
+
+    Furthermore, parent profile has a foreign key relationship to:
+    * a list of students
     '''
     user = models.OneToOneField(User)
-    # student
+
 
 class Course(model.Model):
     '''
     A course has:
     * a name
     * a description
+
+    Furthermore, a course is linked by a many-to-many relationship with:
     * a list of current students
     * a list of dropped students
     '''
     pass
     # name
     # description
-    # students
-    # dropped students
+
+
+class Grade(model.Grade):
+    '''
+    A regular school grade to be used in student profiles.
+    '''
+    pass
+    # name
 
