fd6484ef04325be69fa6aa00df2e96f062d3e724
Emmanuel <info@regularoddity.com>
2013-10-26 22:24:17 -0700

    New profile system

---
+++ dd_portal/migrations/0008_auto__del_parent__del_parentprofile__del_studentprofile__del_progress_.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'Parent'
+        db.delete_table(u'dd_portal_parent')
+
+        # Removing M2M table for field groups on 'Parent'
+        db.delete_table(db.shorten_name(u'dd_portal_parent_groups'))
+
+        # Removing M2M table for field user_permissions on 'Parent'
+        db.delete_table(db.shorten_name(u'dd_portal_parent_user_permissions'))
+
+        # Deleting model 'ParentProfile'
+        db.delete_table(u'dd_portal_parentprofile')
+
+        # Deleting model 'StudentProfile'
+        db.delete_table(u'dd_portal_studentprofile')
+
+        # Deleting model 'Progress'
+        db.delete_table(u'dd_portal_progress')
+
+        # Deleting model 'Student'
+        db.delete_table(u'dd_portal_student')
+
+        # Removing M2M table for field groups on 'Student'
+        db.delete_table(db.shorten_name(u'dd_portal_student_groups'))
+
+        # Removing M2M table for field user_permissions on 'Student'
+        db.delete_table(db.shorten_name(u'dd_portal_student_user_permissions'))
+
+
+    def backwards(self, orm):
+        # Adding model 'Parent'
+        db.create_table(u'dd_portal_parent', (
+            ('username', self.gf('django.db.models.fields.CharField')(max_length=30, unique=True)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+        ))
+        db.send_create_signal(u'dd_portal', ['Parent'])
+
+        # Adding M2M table for field groups on 'Parent'
+        m2m_table_name = db.shorten_name(u'dd_portal_parent_groups')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('parent', models.ForeignKey(orm[u'dd_portal.parent'], null=False)),
+            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['parent_id', 'group_id'])
+
+        # Adding M2M table for field user_permissions on 'Parent'
+        m2m_table_name = db.shorten_name(u'dd_portal_parent_user_permissions')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('parent', models.ForeignKey(orm[u'dd_portal.parent'], null=False)),
+            ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['parent_id', 'permission_id'])
+
+        # Adding model 'ParentProfile'
+        db.create_table(u'dd_portal_parentprofile', (
+            ('notes', self.gf('tinymce.models.HTMLField')(blank=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('parent', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['dd_portal.Parent'], unique=True, null=True)),
+            ('ice_contact', self.gf('django.db.models.fields.CharField')(default='<MISSING>', max_length=255)),
+        ))
+        db.send_create_signal(u'dd_portal', ['ParentProfile'])
+
+        # Adding model 'StudentProfile'
+        db.create_table(u'dd_portal_studentprofile', (
+            ('school_grade', self.gf('django.db.models.fields.CharField')(default='active', max_length=2)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal(u'dd_portal', ['StudentProfile'])
+
+        # Adding model 'Progress'
+        db.create_table(u'dd_portal_progress', (
+            ('status', self.gf('django.db.models.fields.CharField')(default='05', max_length=16)),
+            ('course', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['dd_portal.Course'])),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('student', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['dd_portal.StudentProfile'])),
+        ))
+        db.send_create_signal(u'dd_portal', ['Progress'])
+
+        # Adding model 'Student'
+        db.create_table(u'dd_portal_student', (
+            ('profile', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['dd_portal.StudentProfile'], unique=True)),
+            ('username', self.gf('django.db.models.fields.CharField')(max_length=30, unique=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['dd_portal.Parent'], null=True)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+        ))
+        db.send_create_signal(u'dd_portal', ['Student'])
+
+        # Adding M2M table for field groups on 'Student'
+        m2m_table_name = db.shorten_name(u'dd_portal_student_groups')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('student', models.ForeignKey(orm[u'dd_portal.student'], null=False)),
+            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['student_id', 'group_id'])
+
+        # Adding M2M table for field user_permissions on 'Student'
+        m2m_table_name = db.shorten_name(u'dd_portal_student_user_permissions')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('student', models.ForeignKey(orm[u'dd_portal.student'], null=False)),
+            ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['student_id', 'permission_id'])
+
+
+    models = {
+        u'dd_portal.course': {
+            'Meta': {'object_name': 'Course'},
+            'description': ('tinymce.models.HTMLField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['dd_portal']
--- dd_portal/models.py
+++ dd_portal/models.py
@@ -1,8 +1,6 @@
 from django.db                  import models
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 from tinymce.models             import HTMLField
-from django.db.models.signals   import post_save
-from django.core.exceptions     import DoesNotExist
 
 # Note:
 # A standard Django user contains by default:
@@ -29,47 +27,42 @@ class Course(models.Model):
         return self.name
 
 
-class Parent(AbstractUser):
+class DragonUser(AbstractUser):
     '''
     A  parent profile has a foreign key relationship to:
     * a list of students
     '''
-    #objects = ParentManager()
-    # parentprofile = models.OneToOneField(ParentProfile, editable=False, null=True)
+    USER_TYPE_CHOICES = (
+        ('admin'  , 'Administrator'),
+        ('parent' , 'Parent'       ),
+        ('student', 'Student'      ),
+    )
+    type = models.CharField('Type', max_length=16,
+        choices=USER_TYPE_CHOICES, default='admin')
 
     def save(self, force_insert=False, force_update=False):
         super(AbstractUser, self).save(force_insert, force_update)
-        try:
-            self.parentprofile
-        except ParentProfile.DoesNotExist:
-            profile = ParentProfile()
-            profile.save()
-            self.parentprofile = profile
-        super(AbstractUser, self).save(force_insert, force_update)
+        if self.type == 'admin': return
+        profileType = globals()[capitalize(self.type) + 'Profile']
+        myProfile = profileType.objects.filter(user_id=self.id)
+        if not len(myProfile): myProfile.create(user=self.id)
 
-    class Meta:
-        verbose_name = 'Parent'
 
 class ParentProfile(models.Model):
     '''
-    A  parent profile has a foreign key relationship to:
-    * a list of students
+
     '''
-    parent      = models.OneToOneField(Parent, editable=False, null=True)
+    user        = models.OneToOneField(DragonUser, editable=False, null=True)
     ice_contact = models.CharField('In case of emergency', max_length=255, default='<MISSING>')
     notes       = HTMLField('General notes', blank=True)
 
     def __unicode__(self):
-        return self.parent.full_name() or self.parent.username()
+        return self.user.full_name() or self.user.username()
 
 
 class StudentProfile(models.Model):
     '''
-    A student profile has:
-    * a link to a Django user
-    * a single parent account linked to it
-    * any number of courses
-    * a school grade from a list of choices
+
     '''
     SCHOOL_GRADE_CHOICES = (
         ('05', 'Middle Freshman' ),
@@ -81,32 +74,14 @@ class StudentProfile(models.Model):
         ('11', 'High Junior'     ),
         ('12', 'High Senior'     ),
     )
-    #courses      = models.ManyToManyField(Course, through='Progress')
+    user         = models.OneToOneField(DragonUser, editable=False, null=True)
+    courses      = models.ManyToManyField(Course, through='Progress')
+    parent       = models.ForeignKey(ParentProfile, null=True)
     school_grade = models.CharField('School grade', \
         max_length=2, choices=SCHOOL_GRADE_CHOICES, default='active')
 
     def __unicode__(self):
-        return self.student.full_name() or self.student.username()
-
-class Student(AbstractUser):
-    '''
-    A student profile has:
-    * a link to a Django user
-    * a single parent account linked to it
-    * any number of courses
-    * a school grade from a list of choices
-    '''
-    profile = models.OneToOneField(StudentProfile, editable=False)
-    parent  = models.ForeignKey(Parent, null=True)
-
-    @classmethod
-    def create(cls, username, email, password, parent):
-        student = cls(username, email, password, parent)
-        student.profile = StudentProfile.create(student=student)
-        return student
-
-    class Meta:
-        verbose_name = 'Student'
+        return self.user.full_name() or self.user.username()
 
 
 class Progress(models.Model):
