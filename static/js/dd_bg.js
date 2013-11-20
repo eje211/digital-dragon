834b808e6ac4283d420f29da594a7b9d798fadb1
Emmanuel Eytan <e@regularoddity.com>
2013-11-19 23:05:02 -0800

    Documented the background JavaScript file.

--- static/js/dd_bg.js
+++ static/js/dd_bg.js
@@ -1,4 +1,8 @@
-// Based on: <http://trendmedia.com/news/infinite-rotating-images-using-jquery-javascript/>
+/**
+ * Rotate the top banner picture on the index page.
+ * At each rotation, also swipe in and out the banner text.
+ * Based on: <http://trendmedia.com/news/infinite-rotating-images-using-jquery-javascript/>
+ */
 var InfiniteRotator = {
     init: function() {
         //initial fade-in time (in milliseconds)
@@ -35,20 +39,39 @@ var InfiniteRotator = {
     }
 };
 
+/**
+ * Gather the size of the background image of a div so that
+ * it can then be resized accordingly.
+ *
+ * var elem: the element to be resized.
+ * var url : the URL of that element's background image.
+ *
+ * NOTE: The url variable could be gathered from the element's,
+ * CSS properties but it's simpler to just keep it in
+ * window.backgrounds and pass it when needed.
+ */
 var set_img_data = function(elem, url) {
     // Only refresh image data if we have to.
-    if (set_img_data.url === url) return resize_bg(elem);
+    if (set_img_data.url === url) return _resize_bg(elem);
     set_img_data.url = url;
     var img = new Image();
     img.onload = function() {
         set_img_data.bg_width = this.width;
         set_img_data.bg_ratio = this.width / this.height;
-        resize_bg(elem);
+        _resize_bg(elem);
     }
     img.src = url;
 }
 
-var resize_bg = function(elem) {
+/**
+ * Resize a background div based on its background-image
+ * property, as supplied by the set_img_data() function.
+ * NOTE: Pseaudo-private function: this function should
+ * only ever be called by set_img_data().
+ * 
+ * var elem: the element to be resized.
+ */
+var _resize_bg = function(elem) {
     var max_height = parseInt($(elem).css('max-height'));
     if ($(window).width() / set_img_data.bg_ratio >= max_height) {
         var width = $(window).width(),
@@ -62,12 +85,21 @@ var resize_bg = function(elem) {
     });
 }
 
+/**
+ * What to do when the main window is resized:
+ * reset the size of the background image of the div accordinly.
+ */
 var resize_callback = function() {
-    resize_callback.current_bg_item = window.current_bg_item;
-
-    set_img_data(window.backgrounds[window.current_bg_item]['element'], window.backgrounds[window.current_bg_item]['url']);
+    set_img_data(
+        window.backgrounds[window.current_bg_item]['element'],
+        window.backgrounds[window.current_bg_item]['url']
+    );
 }
 
+/**
+ * Gather information about the first backrgound item
+ * so that it can be included in the rotation.
+ */
 var init_first_background = function () {
     var first_bg = {
         'url': $('.dd_bg_cont').eq(0).css('background-image').slice(4, -1),
@@ -75,23 +107,31 @@ var init_first_background = function () {
         'link': $('#title-tag p a').attr('href'),
         'element' : $('.dd_bg_cont').eq(0)
     };
-    $('#title-tag p').each(function(key, value) {first_bg['text'][key]= $(value).text();});
+    $('#title-tag p').each(function(key, value) {first_bg['text'][key] = $(value).text();});
     window.backgrounds = [first_bg];
 };
 
+/**
+ * Get data from the JSON file and create new DIVs for the
+ * rotating backgrounds based on it. Also save a record
+ * of that data in window.backgrounds .
+ */
 var init_backgrounds = function() {
     $.getJSON('/static/js/backgrounds.js', function(data) {
         // Add the new backgrounds to the DOM.
         $.each(data, function(key, image) {
+            // For each new entry, create a DIV after the last one.
             $('.dd_bg_cont').last().after('<div class="dd_bg_cont"></div>');
+            // Set its background image.
             $('.dd_bg_cont').last().css('background-image', 'url(' + image['url'] + ')');
             // Split each line of text into an array.
             image['text'] = image['text'].split('\n');
-            // Resize the background image for each new div.
+            // Resize the background image.
             set_img_data($('.dd_bg_cont').last(), image['url']);
         });
+        // Add all the DIVs we've added to window.backgrounds .
         $.merge(window.backgrounds, data);
-        // Set the "element" value to each item in the array.s
+        // Set the "element" value to each item in the array.
         $('.dd_bg_cont').each(function (key, value) {
             window.backgrounds[key]['element'] = value;
         });
@@ -100,6 +140,9 @@ var init_backgrounds = function() {
     });
 }
 
+/**
+ * Main fuction. Start everything.
+ */
 $(function() {
     // Set current image rotation item
     window.current_bg_item = 0;
